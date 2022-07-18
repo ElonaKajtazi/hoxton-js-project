88f54bf1f19e7af6bf1d0ececd2790527ef978b8
ElonaKajtazi <elona.kajtazi@gmail.com>
2022-07-18 14:08:45 +0200

    created some state and a fetch function

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,32 @@
-import './style.css'
+import "./style.css";
 
-function render(){
-  let mainEl=document.querySelector('#app')
-  if (mainEl===null)return
-  mainEl.textContent = ""
-  let h1El=document.createElement('h1')
-  h1El.textContent = "Don't finish it this early"
-  let h2El=document.createElement('h2')
-  h2El.textContent = "Cool, it works!"
-  mainEl.append(h1El, h2El)
+let state = {
+  apiKey: "d8e39a035f63c724b30d68bd39bb4022",
+  city: "new york",
+  weather: {},
+};
+
+function getWeatherDataFromServer() {
+  fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=99e85ef67b8f4f57852185003221707&q=${state.city}&aqi=no`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      state.weather = data;
+    });
 }
-render()
+
+getWeatherDataFromServer();
+window.state = state;
+
+// function render() {
+//   // let mainEl=document.querySelector('#app')
+//   // if (mainEl===null)return
+//   // mainEl.textContent = ""
+//   // let h1El=document.createElement('h1')
+//   // h1El.textContent = "Don't finish it this early"
+//   // let h2El=document.createElement('h2')
+//   // h2El.textContent = "Cool, it works!"
+//   // mainEl.append(h1El, h2El)
+// }
+// render();
