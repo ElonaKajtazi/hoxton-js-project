5ff4fe99e459c67dfd92dfaf45b02e1e28c6455d
Elona Kajtazi <MY_NAME@example.com>
2022-07-18 21:16:02 +0200

    ....

--- src/main.ts
+++ src/main.ts
@@ -40,78 +40,81 @@ type WeatherData = {
     gust_kph: number;
   };
   forecast: {
-    forecastday: {
-      date: string;
-      date_epoch: number;
-      day: {
-        maxtemp_c: number;
-        maxtemp_f: number;
-        mintemp_c: number;
-        mintemp_f: number;
-        avgtemp_c: number;
-        avgtemp_f: number;
-        maxwind_mph: number;
-        maxwind_kph: number;
-        totalprecip_mm: number;
-        totalprecip_in: number;
-        avgvis_km: number;
-        avgvis_miles: number;
-        avghumidity: number;
-        condition: {
-          text: string;
-          icon: string;
-          code: number;
+    forecastday: [
+      {
+        date: string;
+        date_epoch: number;
+        day: {
+          maxtemp_c: number;
+          maxtemp_f: number;
+          mintemp_c: number;
+          mintemp_f: number;
+          avgtemp_c: number;
+          avgtemp_f: number;
+          maxwind_mph: number;
+          maxwind_kph: number;
+          totalprecip_mm: number;
+          totalprecip_in: number;
+          avgvis_km: number;
+          avgvis_miles: number;
+          avghumidity: number;
+          condition: {
+            text: string;
+            icon: string;
+            code: number;
+          };
+          uv: number;
         };
-        uv: number;
-      };
-      astro: {
-        sunrise: string;
-        sunset: string;
-        moonrise: string;
-        moonset: string;
-        moon_phase: string;
-        moon_illumination: number;
-      };
-      hour: {
-        time_epoch: number;
-        time: string;
-        temp_c: number;
-        temp_f: number;
-        is_day: number;
-        condition: {
-          text: string;
-          icon: string;
-          code: number;
+        astro: {
+          sunrise: string;
+          sunset: string;
+          moonrise: string;
+          moonset: string;
+          moon_phase: string;
+          moon_illumination: number;
         };
-        wind_mph: number;
-        wind_kph: number;
-        wind_degree: number;
-        wind_dir: string;
-        pressure_mb: number;
-        pressure_in: number;
-        precip_mm: number;
-        precip_in: number;
-        humidity: number;
-        cloud: number;
-        feelslike_c: number;
-        feelslike_f: number;
-        windchill_c: number;
-        windchill_f: number;
-        heatindex_c: number;
-        heatindex_f: number;
-        dewpoint_c: number;
-        dewpoint_f: number;
-        will_it_rain: number;
-        will_it_snow: number;
-        vis_km: number;
-        vis_miles: number;
-        uv: number;
-        gust_mph: number;
-        gust_kph: number;
-      };
-    };
+        hour: {
+          time_epoch: number;
+          time: string;
+          temp_c: number;
+          temp_f: number;
+          is_day: number;
+          condition: {
+            text: string;
+            icon: string;
+            code: number;
+          };
+          wind_mph: number;
+          wind_kph: number;
+          wind_degree: number;
+          wind_dir: string;
+          pressure_mb: number;
+          pressure_in: number;
+          precip_mm: number;
+          precip_in: number;
+          humidity: number;
+          cloud: number;
+          feelslike_c: number;
+          feelslike_f: number;
+          windchill_c: number;
+          windchill_f: number;
+          heatindex_c: number;
+          heatindex_f: number;
+          dewpoint_c: number;
+          dewpoint_f: number;
+          will_it_rain: number;
+          will_it_snow: number;
+          vis_km: number;
+          vis_miles: number;
+          uv: number;
+          gust_mph: number;
+          gust_kph: number;
+        };
+      }
+    ];
   };
 };
+
 type State = {
   apiKey: string;
   city: string;
@@ -120,7 +123,7 @@ type State = {
 // Kindof created stata, will update it later
 let state = {
   apiKey: "d8e39a035f63c724b30d68bd39bb4022",
-  city: "New york",
+  city: "Dragash",
   weatherData: {},
 };
 // create a function that will get the weather data from the API
@@ -156,7 +159,7 @@ function render() {
 
   let currentTemperatureEl = document.createElement("h2");
   currentTemperatureEl.className = "current-temperature";
-//@ts-ignore
+  //@ts-ignore
   currentTemperatureEl.textContent = state.weatherData["current"].temp_c;
 
   let descriptionEl = document.createElement("div");
