173c9fddd429f52c2ee02dcc2b9a11e1263ef572
ElonaKajtazi <elona.kajtazi@gmail.com>
2022-07-18 10:23:06 +0200

    .....

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ function render(){
   if (mainEl===null)return
   mainEl.textContent = ""
   let h1El=document.createElement('h1')
-  h1El.textContent = "Finishing the project"
+  h1El.textContent = "Don't finish it this early"
   let h2El=document.createElement('h2')
   h2El.textContent = "Cool, it works!"
-  mainEl.append(h1El)
+  mainEl.append(h1El, h2El)
 }
 render()
